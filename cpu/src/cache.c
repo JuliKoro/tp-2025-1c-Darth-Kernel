cab4318b01c6114e75755e617e03b621609c7238
Julian Koroluk <jkoroluk@frba.utn.edu.ar>
2025-08-01 17:58:50 +0000

    Fix cache

--- cpu/src/cache.c
+++ cpu/src/cache.c
@@ -32,8 +32,8 @@ void destruir_cache() {
 
 bool acceder_cache() {
     // Verificar si la caché está habilitada
-    if (cache == NULL || cache->capacidad == 0) {
-        log_debug(logger_cpu, "Caché deshabilitada, accediendo directamente a memoria.");
+    if (cache == NULL || cache->tamanio == 0) {
+        log_debug(logger_cpu, "Caché deshabilitada");
         return false; // La caché está deshabilitada, no se puede acceder
     }
     return true;
